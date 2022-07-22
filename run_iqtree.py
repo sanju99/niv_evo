a05693d6bbe138e19c9dcb3874317b00218845f5
Sanjana <sgkulkarni02@gmail.com>
2022-07-22 00:11:23 -0400

    BGD dedup tree

--- run_iqtree.py
+++ run_iqtree.py
@@ -1,10 +1,18 @@
 import os, subprocess, sys
 
-_, phylip_file, num_bootstrap = sys.argv
-   
-# perform model selection. czb collapses zero branches to allow for polytomies
-#subprocess.run(f"iqtree -s {phylip_file} -m MFP -o JN255817 -czb -redo", shell=True)
-subprocess.run(f"iqtree -s {phylip_file} -m MFP -czb -redo", shell=True)
+if len(sys.argv) == 4:
+    _, phylip_file, num_bootstrap, out_group = sys.argv
+elif len(sys.argv) == 3:
+    _, phylip_file, num_bootstrap = sys.argv
+    out_group = None
+else:
+    raise ValueError("3 or 4 command line arguments must be provided!")
+    
+# perform model selection. czb collapses zero branches to allow for polytomies. Use JN255817 (HeV) for out group
+if out_group is not None:
+    subprocess.run(f"iqtree -s {phylip_file} -m MFP -o {out_group} -czb -redo", shell=True)
+else:
+    subprocess.run(f"iqtree -s {phylip_file} -m MFP -czb -redo", shell=True)
 
 # get the best fit model, which is on the 37th line of the output file. Default selection is minimizing the Bayesian information criterion
 with open(f"{phylip_file}.iqtree") as file:
@@ -14,44 +22,46 @@ best_model = "".join(lines[36].split()).split(":")[1]
 print(f"Selected codon substitution model: {best_model}\n")
 
 # construct the trees using the best model, with bootstrapping
-# subprocess.run(f"iqtree -s {phylip_file} -m {best_model} -b {num_bootstrap} -o JN255817 -czb -redo", shell=True)
-subprocess.run(f"iqtree -s {phylip_file} -m {best_model} -b {num_bootstrap} -czb -redo", shell=True)
-
-# convert bootstrap numbers (95 vs. 0.95) to proportion (proportion of bootstrap replicates that support a split)
-with open(f"{phylip_file}.treefile", "r") as file:
-    lines = file.readlines()
-    lines = [line.strip("\n") for line in lines]
-    
-if len(lines) == 1:
-    lines = lines[0]
+if out_group is not None:
+    subprocess.run(f"iqtree -s {phylip_file} -m {best_model} -b {num_bootstrap} -o {out_group} -czb -redo", shell=True)
 else:
-    raise ValueError(f"More than one line in {phylip_file}.treefile!")
-    
-oldlines_list = lines.split(":")
-newlines = []
+    subprocess.run(f"iqtree -s {phylip_file} -m {best_model} -b {num_bootstrap} -czb -redo", shell=True)
 
-for i, line in enumerate(oldlines_list):
-    if ")" in line:
-        sep = line.split(")")
-        for k, string in enumerate(sep):
-            # excludes decimals and negative numbers. 
-            # Put parentheses back in because the split function removes them
-            if string.isnumeric():
-                sep[k] = ")" + str(int(string) / 100)
-        if i == 0:
-            newlines.append("".join(sep))
-        else:
-            newlines.append(":" + "".join(sep))
-    else:
-        if i == 0:
-            newlines.append(line)
-        else:
-            newlines.append(":" + line)
+# # convert bootstrap numbers (95 vs. 0.95) to proportion (proportion of bootstrap replicates that support a split)
+# with open(f"{phylip_file}.treefile", "r") as file:
+#     lines = file.readlines()
+#     lines = [line.strip("\n") for line in lines]
     
-assert len(newlines) == len(oldlines_list)
+# if len(lines) == 1:
+#     lines = lines[0]
+# else:
+#     raise ValueError(f"More than one line in {phylip_file}.treefile!")
     
-with open(f"{phylip_file}.treefile", "w+") as file:
-    file.write("".join(newlines)[:-1] + ");")
+# oldlines_list = lines.split(":")
+# newlines = []
+
+# for i, line in enumerate(oldlines_list):
+#     if ")" in line:
+#         sep = line.split(")")
+#         for k, string in enumerate(sep):
+#             # excludes decimals and negative numbers. 
+#             # Put parentheses back in because the split function removes them
+#             if string.isnumeric():
+#                 sep[k] = ")" + str(int(string) / 100)
+#         if i == 0:
+#             newlines.append("".join(sep))
+#         else:
+#             newlines.append(":" + "".join(sep))
+#     else:
+#         if i == 0:
+#             newlines.append(line)
+#         else:
+#             newlines.append(":" + line)
+    
+# assert len(newlines) == len(oldlines_list)
+    
+# with open(f"{phylip_file}.treefile", "w+") as file:
+#     file.write("".join(newlines)[:-1] + ");")
     
 # move the iqtree and actual tree files to the trees folder, then delete all the remaining files. Contree is the consensus tree file that contains BS support values
 new_phylip_name = os.path.join("niv_evo/trees", os.path.basename(f"{phylip_file}.iqtree".split(".")[0] + "_iqtree.txt"))
