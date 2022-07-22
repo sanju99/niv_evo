68c15377214e191563173ed9404dd0c5f635170a
Sanjana <sgkulkarni02@gmail.com>
2022-07-22 10:19:03 -0400

    cleaned more sequences

--- run_iqtree.py
+++ run_iqtree.py
@@ -27,41 +27,41 @@ if out_group is not None:
 else:
     subprocess.run(f"iqtree -s {phylip_file} -m {best_model} -b {num_bootstrap} -czb -redo", shell=True)
 
-# # convert bootstrap numbers (95 vs. 0.95) to proportion (proportion of bootstrap replicates that support a split)
-# with open(f"{phylip_file}.treefile", "r") as file:
-#     lines = file.readlines()
-#     lines = [line.strip("\n") for line in lines]
+# convert bootstrap numbers (95 vs. 0.95) to proportion (proportion of bootstrap replicates that support a split)
+with open(f"{phylip_file}.treefile", "r") as file:
+    lines = file.readlines()
+    lines = [line.strip("\n") for line in lines]
     
-# if len(lines) == 1:
-#     lines = lines[0]
-# else:
-#     raise ValueError(f"More than one line in {phylip_file}.treefile!")
+if len(lines) == 1:
+    lines = lines[0]
+else:
+    raise ValueError(f"More than one line in {phylip_file}.treefile!")
     
-# oldlines_list = lines.split(":")
-# newlines = []
+oldlines_list = lines.split(":")
+newlines = []
 
-# for i, line in enumerate(oldlines_list):
-#     if ")" in line:
-#         sep = line.split(")")
-#         for k, string in enumerate(sep):
-#             # excludes decimals and negative numbers. 
-#             # Put parentheses back in because the split function removes them
-#             if string.isnumeric():
-#                 sep[k] = ")" + str(int(string) / 100)
-#         if i == 0:
-#             newlines.append("".join(sep))
-#         else:
-#             newlines.append(":" + "".join(sep))
-#     else:
-#         if i == 0:
-#             newlines.append(line)
-#         else:
-#             newlines.append(":" + line)
+for i, line in enumerate(oldlines_list):
+    if ")" in line:
+        sep = line.split(")")
+        for k, string in enumerate(sep):
+            # excludes decimals and negative numbers. 
+            # Put parentheses back in because the split function removes them
+            if string.isnumeric():
+                sep[k] = ")" + str(int(string) / 100)
+        if i == 0:
+            newlines.append("".join(sep))
+        else:
+            newlines.append(":" + "".join(sep))
+    else:
+        if i == 0:
+            newlines.append(line)
+        else:
+            newlines.append(":" + line)
     
-# assert len(newlines) == len(oldlines_list)
+assert len(newlines) == len(oldlines_list)
     
-# with open(f"{phylip_file}.treefile", "w+") as file:
-#     file.write("".join(newlines)[:-1] + ");")
+with open(f"{phylip_file}.treefile", "w+") as file:
+    file.write("".join(newlines)[:-1] + ");")
     
 # move the iqtree and actual tree files to the trees folder, then delete all the remaining files. Contree is the consensus tree file that contains BS support values
 new_phylip_name = os.path.join("niv_evo/trees", os.path.basename(f"{phylip_file}.iqtree".split(".")[0] + "_iqtree.txt"))
