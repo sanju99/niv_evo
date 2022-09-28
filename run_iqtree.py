1bb0607351ee009c8bc62499dd01bbdf1df5c56c
Sanjana <sgkulkarni02@gmail.com>
2022-09-28 09:52:34 -0400

    removed V/W iqtree

--- run_iqtree.py
+++ run_iqtree.py
@@ -1,18 +1,26 @@
 import os, subprocess, sys
 
-if len(sys.argv) == 4:
-    _, phylip_file, num_bootstrap, out_group = sys.argv
-elif len(sys.argv) == 3:
-    _, phylip_file, num_bootstrap = sys.argv
+if len(sys.argv) == 5:
+    _, phylip_file, num_bootstrap, out_group, dna_protein = sys.argv
+elif len(sys.argv) == 4:
+    _, phylip_file, num_bootstrap, dna_protein = sys.argv
     out_group = None
 else:
-    raise ValueError("3 or 4 command line arguments must be provided!")
+    raise ValueError("4 or 5 command line arguments must be provided!")
+    
+# use protein models if the last argument is protein
+if dna_protein == "protein":
+    model = "-st CODON"
+else:
+    model = ""
+    
     
 # perform model selection. czb collapses zero branches to allow for polytomies. Use JN255817 (HeV) for out group
+# MFP = ModelFinder Plus, which tells IQ-TREE to perform ModelFinder and the remaining analysis using the selected model
 if out_group is not None:
-    subprocess.run(f"iqtree -s {phylip_file} -m MFP -o {out_group} -czb -redo", shell=True)
+    subprocess.run(f"iqtree -s {phylip_file} -m MFP -o {out_group} {model} -czb -redo", shell=True)
 else:
-    subprocess.run(f"iqtree -s {phylip_file} -m MFP -czb -redo", shell=True)
+    subprocess.run(f"iqtree -s {phylip_file} -m MFP {model} -czb -redo", shell=True)
 
 # get the best fit model, which is on the 37th line of the output file. Default selection is minimizing the Bayesian information criterion
 with open(f"{phylip_file}.iqtree") as file:
